d8b81216d44286af98508a013d665b1727838034
Daniele <daniele.fiorenti90@gmail.com>
2014-10-30 14:02:51 +0100

    Added String Reverse function

--- doStringReverse.cc
+++ doStringReverse.cc
@@ -7,7 +7,20 @@
 
 void doStringReverse()
 {
+	std::cout << "String reverse function " << std::endl;
 
+	std::string Stringa = "";
+	std::string reversedString = "";
+
+	std::cout<<"String To Reverse=";
+	std::getline(std::cin,Stringa);
+	//assert(substr.size()>0&&substr.size()<10);
+
+	for (int i = Stringa.size()-1; i >= 0; i--){
+	    reversedString += Stringa[i];
+	}
+
+	std::cout << "Stringa: " << Stringa << " Stringa Rovesciata: " << reversedString << std::endl;
 }
 
 
