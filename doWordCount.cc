fd0d3461afa2b33f20ac8344423bcf91fbc81d38
Andrea Marchese <m.marcheseandrea@gmail.com>
2014-10-30 14:03:26 +0100

    Full function Do Count Words

--- doWordCount.cc
+++ doWordCount.cc
@@ -3,8 +3,32 @@
 
 #include "functions.hh"
 
-void doWordCount()
+int doWordCount( )
 {
+	std::cout<<"\n\n ************ doCountWord ['q' to exit]"<<std::endl;
+	std::string str;
+	int count=0;
+
+	std::cout<<"Stringa=";
+	
+  std::getline(std::cin,str);
+	while(str!="q")
+	{
+		if(str.size()>0){
+		count=1;
+		
+		for(unsigned int i=0;i<(str.size());i++)
+		{
+			if(' '==str.at(i))
+			{
+				count++;
+			}
+		}
+		  std::cout<<str<<" Words Count:"<<count<<std::endl;
+	   }
+       std::getline(std::cin,str);
+	}
+	return count;
 
 }
 
